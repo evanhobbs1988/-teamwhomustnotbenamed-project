5a169cb54a4a58fe95fb4282ea483f90d4287a26
Anna Smith <annamsmith2893@gmail.com>
2017-07-11 21:53:10 -0500

    Added javascriptgit status Notes in both the HTML and js for team review

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var ArpsEquation = function(){
+	
+	IP: document.getElementById('IP');
+
+	b-factor: document.getElementById('b-factor');
+
+	decline: document.getElementById('decline');
+
+	// t: sequence for t goes here 
+
+	ROD: function(){
+		// example of playing with a for loop
+		for(var i = 0; i < this.t.length; i++){
+			// script actually goes in here for ROD equation, will get at work 
+		};
+	}
+
+	// table: function(){
+		// functon goes here that will map our table??
+	// }
+}
+
+ArpsEquation.draw();
