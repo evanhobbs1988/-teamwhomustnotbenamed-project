c12a13aea159d0fd0dfed81ad461033229c6e3ee
Anna Smith <annamsmith2893@gmail.com>
2017-07-13 18:09:29 -0500

    Finally got the javascript workinggit add -A

--- js/app.js
+++ js/app.js
@@ -13,27 +13,40 @@ var ArpsEquation = {
 			return a;
 		};
 
-		var t = range(1,monthCount,1)
+		var t = range(1,monthCount,1);
+
+		return t;
 	},
 
 	ROD: function(){
+		console.log("This called the ROD function");
 		var IP = document.getElementById('IP').value;
+		console.log("IP",IP);
 
 		var bFactor = document.getElementById('bFactor').value;
 
 		var decline = document.getElementById('decline').value;
 
-		var t = month;
+		var t = this.month();
+		console.log("t",t);
 		// example of playing with a for loop
-		for(var i = 0; i < this.t.length; i++){
-			(IP*30.41667)*(1+bFactor*decline*t)^(-1/b)
+		for(var i = t.length-1; i > 0; i--){
+			console.log((IP*30.41667)*(1+bFactor*decline*i)^(-1/bFactor));
 			// script actually goes in here for ROD equation, will get at work 
 		};
-	}
+	},
+
+
+	[{
+		t: "number",
+		ROD: "number"
+	},
 
 	// table: function(){
-		// functon goes here that will map our table??
+		// functon goes here that will map our table?? need it to return 2 columns - a column of t values, 
+		// corresponding with a column of ROD values
+		// use... d3??? 
 	// }
 }
 
-ArpsEquation.draw();
+// ArpsEquation.draw();
