cba7ec639bc5f3e4191811e5b86dd9a9350b874a
Anna Smith <annamsmith2893@gmail.com>
2017-07-20 17:49:32 -0500

    Huge strides on the jsgit status

--- js/app.js
+++ js/app.js
@@ -31,8 +31,20 @@ var ArpsEquation = {
 		var t = this.month();
 		
 		// for loop that has all the ARPS equations piled on top of one another
-		for(var i = t.length-1; i > 0; i--){
-			console.log((IP*30.41667)*(1+bFactor*decline*i)^(-1/bFactor));
+		// for(var i = t.length-1; i > 0; i--){
+		for(var i = 1; i < t.length; i++){
+			// recalculate the decline to be nominal decline
+			var DiH = 1-Math.exp(((1-Math.pow(1-decline, -bFactor))/bFactor));
+			var nominalDecline = -Math.log(1-DiH)/12;
+			
+			var ROD1 = (IP*30.41667)*Math.pow((1+bFactor*nominalDecline*i),(-1/bFactor));
+			var ROD2 = (IP*30.41667)*Math.pow((1+bFactor*nominalDecline*(i+1)),(-1/bFactor));
+			var ED = -(Math.pow(ROD1/(IP*30.41667),bFactor)-1)/(bFactor*i);
+			var CUM1 = (IP*30.41667)/((nominalDecline)*(1-bFactor))*(1-Math.pow((ROD1/(IP*30.41667)),(1-bFactor)));
+			var CUM2 = (IP*30.41667)/((nominalDecline)*(1-bFactor))*(1-Math.pow((ROD2/(IP*30.41667)),(1-bFactor)));
+			var HYPER = CUM2 - CUM1;
+			var ARRAY = [i, HYPER];
+			console.log(ARRAY);		
 		};
 	}
 
