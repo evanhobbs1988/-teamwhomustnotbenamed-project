8e58e79573af3e5893e7724ae69f34d270cad2b6
Anna Smith <annamsmith2893@gmail.com>
2017-07-25 17:53:25 -0500

    pushed js changes

--- js/app.js
+++ js/app.js
@@ -32,19 +32,26 @@ var ArpsEquation = {
 		
 		// for loop that has all the ARPS equations piled on top of one another
 		// for(var i = t.length-1; i > 0; i--){
-		for(var i = 1; i < t.length; i++){
+		for(var i = 1; i <= t.length; i++){
 			// recalculate the decline to be nominal decline
 			var DiH = 1-Math.exp(((1-Math.pow(1-decline, -bFactor))/bFactor));
 			var nominalDecline = -Math.log(1-DiH)/12;
 			
-			var ROD1 = (IP*30.41667)*Math.pow((1+bFactor*nominalDecline*i),(-1/bFactor));
-			var ROD2 = (IP*30.41667)*Math.pow((1+bFactor*nominalDecline*(i+1)),(-1/bFactor));
+			// need to check if these are calculating daily or monthly values!!
+			var ROD1 = (IP*30.41667)*Math.pow((1+bFactor*nominalDecline*(i-1)),(-1/bFactor));
+			var ROD2 = (IP*30.41667)*Math.pow((1+bFactor*nominalDecline*(i)),(-1/bFactor));
 			var ED = -(Math.pow(ROD1/(IP*30.41667),bFactor)-1)/(bFactor*i);
 			var CUM1 = (IP*30.41667)/((nominalDecline)*(1-bFactor))*(1-Math.pow((ROD1/(IP*30.41667)),(1-bFactor)));
 			var CUM2 = (IP*30.41667)/((nominalDecline)*(1-bFactor))*(1-Math.pow((ROD2/(IP*30.41667)),(1-bFactor)));
 			var HYPER = CUM2 - CUM1;
-			var ARRAY = [i, HYPER];
-			console.log(ARRAY);		
+			// modified ARPS changes to exponential decline at a terminal decline of .006
+			if (ED <= .006 && ED > 0){
+				var ARPS = [i, "EXPONENTIAL"];
+			} else {
+				var ARPS = [i, HYPER];
+			}
+			console.log(ARPS);	
+
 		};
 	}
 
