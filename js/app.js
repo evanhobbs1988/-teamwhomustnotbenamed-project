ddc01351b5a5a544491e873bfd58dc2906f18f50
Anna Smith <annamsmith2893@gmail.com>
2017-08-01 18:20:04 -0500

    merging kailee and I's changes both to HTML and js

--- js/app.js
+++ js/app.js
@@ -1,47 +1,48 @@
 var dataArray=[];
+var ARPSEUR =[];
 
 google.charts.load('current', {'packages':['line']});
 google.charts.load('current', {'packages':['table']});
-    // google.charts.setOnLoadCallback(drawChart);
+   // google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
+   function drawChart() {
 
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Month');
-      data.addColumn('number', 'Production');
+     var data = new google.visualization.DataTable();
+     data.addColumn('number', 'Month');
+     data.addColumn('number', 'Production');
 
-      data.addRows(dataArray);
+     data.addRows(dataArray);
 
-      var options = {
-        chart: {
-          title: 'Production Forecast',
-          subtitle: 'Mcf or b per month'
-        },
-        legend: {
-        	position: 'none'
-        }
-        
-      };
+     var options = {
+       chart: {
+         title: 'Production Forecast',
+         subtitle: 'Mcf or b per month'
+       },
+       legend: {
+           position: 'none'
+       }
+       
+     };
 
-      var chart = new google.charts.Line(document.getElementById('line-chart'));
+     var chart = new google.charts.Line(document.getElementById('line-chart'));
 
-      chart.draw(data, google.charts.Line.convertOptions(options));
-    }
-	
-	function drawTable() {
+     chart.draw(data, google.charts.Line.convertOptions(options));
+   }
+    
+    function drawTable() {
 
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Month');
-      data.addColumn('number', 'Production');
+     var data = new google.visualization.DataTable();
+     data.addColumn('number', 'Month');
+     data.addColumn('number', 'Production');
 
-      console.log("data array inside draw chart",dataArray)
+     console.log("data array inside draw chart",dataArray)
 
-      data.addRows(dataArray);
+     data.addRows(dataArray);
 
-      var table = new google.visualization.Table(document.getElementById('data-table'));
+     var table = new google.visualization.Table(document.getElementById('data-table'));
 
-      table.draw(data, {width: '100%', height: '100%'});
-    }
+     table.draw(data, {width: '100%', height: '100%'});
+   }
 
 var ArpsEquation = {
 
@@ -65,6 +66,7 @@ var ArpsEquation = {
 	Forecast: function(){
 
 		dataArray = [];
+		ARPSEUR = [];
 
 		// console.log("This called the ROD function");
 		var IP = document.getElementById('IP').value;
@@ -90,16 +92,47 @@ var ArpsEquation = {
 			var CUM2 = (IP*30.41667)/((nominalDecline)*(1-bFactor))*(1-Math.pow((ROD2/(IP*30.41667)),(1-bFactor)));
 			var HYPER = CUM2 - CUM1;
 			// modified ARPS changes to exponential decline at a terminal decline of .006
-			if (ED <= .006 && ED > 0){
-				var ARPS = [i, ED];
+			if (ED <= .06/12 && ED > 0){
+				var ARPS = [i, HYPER];
 			} else {
 				var ARPS = [i, HYPER];
 			}
 			dataArray.push(ARPS);	
 
 		};
-		drawChart()
-		drawTable()
-		console.log(dataArray);
+		drawChart();
+        drawTable();
+        console.log(dataArray);
+
+		for(var i = 1; i <= 600; i++){
+			// recalculate the decline to be nominal decline
+			var DiH = 1-Math.exp(((1-Math.pow(1-decline, -bFactor))/bFactor));
+			var nominalDecline = -Math.log(1-DiH)/12;
+			
+			// need to check if these are calculating daily or monthly values!!
+			var ROD1 = (IP*30.41667)*Math.pow((1+bFactor*nominalDecline*(i-1)),(-1/bFactor));
+			var ROD2 = (IP*30.41667)*Math.pow((1+bFactor*nominalDecline*(i)),(-1/bFactor));
+			var ED = -(Math.pow(ROD1/(IP*30.41667),bFactor)-1)/(bFactor*i);
+			var CUM1 = (IP*30.41667)/((nominalDecline)*(1-bFactor))*(1-Math.pow((ROD1/(IP*30.41667)),(1-bFactor)));
+			var CUM2 = (IP*30.41667)/((nominalDecline)*(1-bFactor))*(1-Math.pow((ROD2/(IP*30.41667)),(1-bFactor)));
+			var HYPER = CUM2 - CUM1;
+			// modified ARPS changes to exponential decline at a terminal decline of .006
+			if (ED <= .06/12 && ED > 0){
+				var ARPS600 = HYPER;
+			} else {
+				var ARPS600 = HYPER;
+			}
+			ARPSEUR.push(ARPS600);	
+		};
+
+		console.log(ARPSEUR);
+
+		function add(a,b) {
+			return a + Math.round(b);
+		}
+
+		var sum = ARPSEUR.reduce(add,0);
+
+		document.getElementById("EUR").innerHTML = sum;
 	}
 }
